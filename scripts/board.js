4203b77d6af1b62f23baf539f8a7a36a470d9eb3
fl4ntn <flynn.anton@gmx.de>
2025-02-23 19:00:50 +0100

    board JS angefangen und Ordnerstruktur kreiert
    Ich habe eine Ornerstruktur für die Tasks gemacht, damit wir sie auf den Server laden können und auch wieder rausholen können

---
+++ scripts/board.js
@@ -0,0 +1,55 @@
+const Base_URL = "https://joinstorage-805e6-default-rtdb.europe-west1.firebasedatabase.app/";
+
+let currentTasks = [];
+let currentTask = {};
+let taskId = "";
+
+async function loadTaskData() {
+    let TaskResponse = await fetch(Base_URL + "/tasks/" + ".json");
+    TaskResponseToJson = await TaskResponse.json();
+
+   for (let index = 0; index < Object.values(TaskResponseToJson).length; index++) {
+        currentTasks.push(Object.values(TaskResponseToJson)[index]);
+   }    
+
+   taskId = Object.values(TaskResponseToJson).length;
+   updateTaskBoard();
+}
+
+function updateTaskBoard() {
+    //hier geben wir die Daten in ein Template dass dan als task-card im Board angezeigt wird
+}
+
+function pushTaskToServer() {
+    getTaskInfoFromUser();
+    postTaskData(`/tasks/${taskId}`, currentTask);
+}
+
+
+// Hier dann bitte Infos reinspeichern
+function getTaskInfoFromUser() { 
+    currentTask = {
+        title: "Hier speichern wir den Titel rein",
+        description: "Hie speichern wir die Erklärung über den Task rein",
+        contacts: ["Das ist", "ein Array", "das alle Kontakte enthält", "die an dem Task mitarbeiten"],
+        deadline: "Das ist das Deadline-Datum",
+        prio: "medium",
+        category: "Technica Task",
+        subtasks: ["Das ist die Liste", "an Subtasks die zu erledigen sind"],
+    }
+}
+
+async function postTaskData(path = "", task) {
+
+    let CurrentTaskResponse = await fetch(Base_URL + path + ".json",{
+        method: "PUT",
+        header: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(task)
+    });
+
+    return CurrentTaskResponseToJson = await CurrentTaskResponse.json();
+}
+
+//!!!!Eine Delete-Funktion muss dann immer die Reihenfolge beachten also man muss dannn quasi alle Tasks noch mal neu auf den Server überschreiben, damit die nicht durcheinander geraten!!!!
