2fd3f70674ece9c605548d11f2d6d6938f7b1a19
Bockinger <cbock2011@gmail.com>
2025-02-26 20:21:05 +0100

    addtask continued working on adtask

--- scripts/task/taskMain.js
+++ scripts/task/taskMain.js
@@ -5,22 +5,38 @@ let taskSubTaskArray=[];
 
 
 //Start function
-function init(){
+window.onload= function init(){
+   
+  startAddTask();
     console.log("Starte task");
     loadDataFirebase();
-     taskRenderContact();
+    // taskRenderContact();
+
+}
+
+/*Tasten Clear und Create Task sperren*/ 
+function startAddTask(){
+  document.getElementById('btnCreateTask').classList.add('btn_lockout');
+  document.getElementById('btnClearTask').classList.add('btn_lockout');
 }
 
 
 
-function taskReadinArray(taskData){
+//Felder prüfen ob Eingabe erfolgte
+function  requiredInputAddTask(){
 
+
+}
+
+
+
+
+function taskReadinArray(taskData){
     console.log("Einträge ",Object.values(taskData).length);
     console.log("Category ",taskData[0].category);
     console.log("deadline ",taskData[0].deadline);
 
-
-    let contactArray=taskData[0].contacts.map(task=>task);
+     let contactArray=taskData[0].contacts.map(task=>task);
 
     console.log("Anzahl der Kontake ",contactArray.length);
      console.log("Kontake ",contactArray);   
@@ -30,7 +46,30 @@ function taskReadinArray(taskData){
 
 
 
-function contactReadinArray(){}
+function contactReadinArray(id,idMsg){
+
+let  input = document.getElementById(id);
+let  taskErrorMsg= document.getElementById(idMsg) 
+
+input.addEventListener("blur",function(){
+
+if(input.value.trim()===""){
+  
+
+}else{
+
+}
+
+
+});
+
+
+}
+
+
+
+
+
 
 
 
