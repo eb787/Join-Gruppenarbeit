e8a7aed0e98cd6c096c2c3218c0fdc29cdd7ac04
Bockinger <cbock2011@gmail.com>
2025-02-27 22:47:19 +0100

    addtask check inputs and selection

--- scripts/task/taskMain.js
+++ scripts/task/taskMain.js
@@ -11,6 +11,8 @@ window.onload= function init(){
     console.log("Starte task");
     loadDataFirebase();
     // taskRenderContact();
+    requiredInputAddTask();
+    openDatePicker();
 
 }
 
@@ -22,11 +24,72 @@ function startAddTask(){
 
 
 
-//Felder prüfen ob Eingabe erfolgte
+//Felder prüfen ob Eingabe erfolgte mit QuerySelector
 function  requiredInputAddTask(){
+  document.querySelectorAll(".input-field").forEach(input =>{
+      input.addEventListener("blur",function(){
+        let errorMsg= this.nextElementSibling;
+        if (this.value.trim()===""){
+        errorMsg.textContent="This field is required";
+          console.log("Fehler");
+               }else{
+          errorMsg.textContent=" ";
+          console.log("Kein Fehler");
+        }
+      })
+  })
+}
 
+
+//Datum Picker öffnen
+function openDatePicker() {
+  document.querySelector(".date-icon").addEventListener("click", function () {
+    let dateInput = document.querySelector(".date-input");
+           
+    if (dateInput.showPicker) {
+        dateInput.showPicker(); // Moderne Browser
+    } else {
+        dateInput.focus(); // Fallback für ältere Browser
+    }
+    
+});
+}
+
+
+function btnPrioSelect(btnPrio){
+  document.querySelectorAll('.btn_prio').forEach(button => {
+    button.style.backgroundColor = "white";
+    button.style.color="black";
+});
+
+
+if(btnPrio=="urgent"){
+  btnPrioBtnSelect("button-urgent","#FF3D00",0)
+}
+if(btnPrio=="medium"){
+  btnPrioBtnSelect("button-medium","#FFA800",1)
+}
+if(btnPrio=="low")  {
+  btnPrioBtnSelect("button-low","#7AE229",2)
+}
+}
+
+
+function btnPrioBtnSelect(auswahl,btnColor){
+  Object.assign(document.getElementsByClassName(auswahl)[0].style,{
+    backgroundColor:btnColor,
+    color:"white",
+   })
+   //document.getElementsByClassName('prio_urgent_img')[id].style.filter = "brightness(0) invert(1)";
+
+   document.querySelectorAll(".prio_urgent_img").forEach(el=>{
+    el.classList.toggle('prio_img_with');
+   })
 
 }
+
+
+
 
 
 
@@ -46,25 +109,13 @@ function taskReadinArray(taskData){
 
 
 
-function contactReadinArray(id,idMsg){
 
-let  input = document.getElementById(id);
-let  taskErrorMsg= document.getElementById(idMsg) 
+   function contactReadinArray(){
 
-input.addEventListener("blur",function(){
-
-if(input.value.trim()===""){
-  
-
-}else{
-
-}
+   }
 
 
-});
 
-
-}
 
 
 
