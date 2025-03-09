be1dd619a3153dc612e337bb56a71cdd98b800df
eb.developer87@gmail.com <eb.developer87@gmail.com>
2025-03-09 17:35:42 +0100

    change name file summary

---
+++ scripts/summary/summary_template.js
@@ -0,0 +1,7 @@
+function addUserLogoTemplate(){
+    return ` 
+        <div class="user_logo">
+        <span id="render_initials_user_logo" onload="renderInitials()"> </span>
+        </div>
+    `
+}
