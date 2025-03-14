3e5bfe71b89b527eb343bb08162654b61045866b
eb.developer87@gmail.com <eb.developer87@gmail.com>
2025-03-14 16:28:41 +0100

    added sign in logo and submenu display none

--- scripts/script.js
+++ scripts/script.js
@@ -9,9 +9,51 @@ const contactColorArray = [
     " #00BEE8",
     " #FF7A00",
      "#040404"
- 
 ]
 
 function toggleMenu() {
     document.getElementById('submenu_toogle').classList.toggle('submenu_open');
+  }
+  
+  document.addEventListener('click', function(event) {
+    const submenu = document.getElementById('submenu_toogle');
+    const toggleButton = document.getElementById('logo_user_sign_in');
+  
+    if (!submenu.contains(event.target) && !toggleButton.contains(event.target)) {
+      submenu.classList.remove('submenu_open'); 
+    }
+  });
+  
+
+function renderInitials() {
+  const Base_URL = "https://joinstorage-805e6-default-rtdb.europe-west1.firebasedatabase.app/";
+  renderUserLogo()
+
+  fetch(`${Base_URL}/currentUser.json`)
+      .then(res => res.json())
+      .then(data => {
+          const userName = data.name; 
+          const initials = getInitials(userName);
+          
+          document.getElementById('render_initials_user_logo').textContent = initials;
+      })
+      .catch(err => console.error("Fehler beim Abrufen des Namens:", err));
+}
+
+function getInitials(name) {
+    const nameParts = name.split(" ");
+    const initials = nameParts.map(part => part.charAt(0).toUpperCase()).join('');
+    return initials;
+}
+
+function renderUserLogo(){
+    document.getElementById("logo_user_sign_in").innerHTML = addUserLogoTemplate();
+    }
+
+function addUserLogoTemplate(){
+    return ` 
+        <div class="user_logo">
+        <div id="render_initials_user_logo"> </div>
+        </div>
+    `
 }
