b02923dde25f81bcdf81bd77f8ac406d3a4b0135
Oliver Plit <oliverplit2005@gmail.com>
2025-03-21 17:33:00 +0100

    merge

--- scripts/contact/contactTemplate.js
+++ scripts/contact/contactTemplate.js
@@ -1,15 +1,13 @@
-function contactCardScrollList(newContact, contactsId, color ) {
+function contactCardScrollList(newContact, contactsId, color, letter ) {
     let nameParts = newContact.name.trim().split(" "); 
     let initials = nameParts[0].charAt(0).toUpperCase(); 
     let backgroundColor = color || "#ccc";
-
     if (nameParts.length > 1) {
         initials += nameParts[1].charAt(0).toUpperCase(); 
     }
 
     return `
-    
-        <div class="contact_Detail_List" id="contact-card-${contactsId}" onclick="openContactBigMiddle('${contactsId}')">
+        <div class="contact_Detail_List" id="contact-card-${contactsId}" onclick="openContactBigMiddle('${contactsId}',letter)">
             <h4 class="contact_abbreviation" style="background-color:${backgroundColor};">${initials}</h4>
             <div>
                 <h2 class="contact_name">${newContact.name}</h2>
@@ -20,7 +18,6 @@ function contactCardScrollList(newContact, contactsId, color ) {
 }
 
 
-
 function contactCardMiddle(user, contactIndex, firstLetter, color) {
     let nameParts = user.name.trim().split(" "); 
     let initials = nameParts[0].charAt(0).toUpperCase(); 
@@ -28,7 +25,6 @@ function contactCardMiddle(user, contactIndex, firstLetter, color) {
     if (nameParts.length > 1) {
         initials += nameParts[1].charAt(0).toUpperCase(); 
     }
-
     return `
         <div class="contact_Detail_Frame" >
             <h4 class="contact_Detail_abbreviation" style="background-color: ${backgroundColor};">${initials}</h4>
@@ -52,10 +48,12 @@ function contactCardMiddle(user, contactIndex, firstLetter, color) {
     `;
 }
 
+
 function contactcardHeadline(mode) {
     let headline = document.getElementById('headline');
     headline.innerHTML = cardNewContact(mode);
 }
+
 
 function cardNewContact(mode) {
     let title = mode === "edit" ? "Edit Contact" : "Add Contact";
@@ -65,22 +63,21 @@ function cardNewContact(mode) {
             <p>${title}</p>
             <span>${text}</span>
             <hr class= "line-contact">
-        
     `;
 }
+
 
 function contactcardHeadlineEdit() {
     let headline = document.getElementById('headline');
     headline.innerHTML = cardNewContactEdit();
 }
 
+
 function cardNewContactEdit() {
     let title = "Edit Contact";
     return `
             <img src="../assets/icons/logocontact.svg">
             <p>${title}</p>
-            
-        
     `;
 }
 
@@ -88,8 +85,8 @@ function cardNewContactEdit() {
 function  alertSuccess() {
     return `
     <div id ="alert">
-    <div class="alert-div">
-  <p class="alert-text">Contact succesfully created</p>  
-</div>
-</div>`
+        <div class="alert-div">
+            <p class="alert-text">Contact succesfully created</p>  
+        </div>
+    </div>`
 }
