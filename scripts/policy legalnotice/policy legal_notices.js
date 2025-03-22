fa72b1b55b791a7ed1677f0d063f0e9c74e662cb
eb.developer87@gmail.com <eb.developer87@gmail.com>
2025-03-22 18:33:45 +0100

    added linking policy legal_notice

--- scripts/policy legalnotice/policy legal_notices.js
+++ scripts/policy legalnotice/policy legal_notices.js
@@ -1,11 +1,27 @@
 function openPageLogin(){
-    window.location.href = "../HTML/login.html"; 
-  }
-  
-  function openPagePolicy(){
-    window.location.href = "../HTML/guest_policy.html"; 
-  }
-  
-  function openPageLegalNotice(){
-    window.location.href = "../HTML/guest_legal_notice.html"; 
-  }
+  window.location.href = "../HTML/login.html"; 
+}
+
+function openPagePolicy(){
+  window.location.href = "../HTML/guest_policy.html"; 
+}
+
+function openPageLegalNotice(){
+  window.location.href = "../HTML/guest_legal_notice.html"; 
+}
+
+function openSummary(){
+  window.location.href = "../HTML/summary.html"; 
+}
+
+function openTask(){
+  window.location.href = "../HTML/task.html"; 
+}
+
+function openBoard(){
+  window.location.href = "../HTML/board.html"; 
+}
+
+function openContact(){
+  window.location.href = "../HTML/contacts.html"; 
+}
