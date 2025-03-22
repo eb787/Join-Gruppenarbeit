a166653d1fbf75bb5971ec026e01b0f0aef002dc
eb.developer87@gmail.com <eb.developer87@gmail.com>
2025-03-22 18:15:40 +0100

    added policy legal_notice mobile

---
+++ scripts/policy legalnotice/policy legal_notices.js
@@ -0,0 +1,11 @@
+function openPageLogin(){
+    window.location.href = "../HTML/login.html"; 
+  }
+  
+  function openPagePolicy(){
+    window.location.href = "../HTML/guest_policy.html"; 
+  }
+  
+  function openPageLegalNotice(){
+    window.location.href = "../HTML/guest_legal_notice.html"; 
+  }
