9f85cc3073d91230fdb7e57892144e1508b2ebb8
eb.developer87@gmail.com <eb.developer87@gmail.com>
2025-04-01 18:08:17 +0200

    welcome guest login

---
+++ scripts/login/login_guest.js
@@ -0,0 +1,40 @@
+async function guestLogin() {
+    if (!localStorage.getItem('userLoggedIn')) {
+        localStorage.setItem('userLoggedIn', 'true');
+    }
+    if (!localStorage.getItem('greetingShown')) {
+        localStorage.setItem('greetingShown', 'false');
+    }
+    setGreetingFlagGuest();
+}
+
+function setGreetingFlagGuest() {
+    openGuestLoginPage();
+}
+
+
+
+function openGuestLoginPage() {
+    try {
+        const url = "../HTML/summary_guest.html";
+        if (url) {
+            window.location.href = url;
+        } else {
+            throw new Error('Ungültige URL');
+        }
+    } catch (error) {
+        console.error("Fehler bei der Weiterleitung:", error);
+    }
+}
+
+
+async function guestLogin() {
+    if (localStorage.getItem('userLoggedIn') === 'true') {
+        console.log("Der Benutzer ist bereits eingeloggt.");
+        return; 
+    }
+    localStorage.setItem('userLoggedIn', 'true');
+    localStorage.setItem('greetingShown', 'false');
+    setGreetingFlagGuest();
+}
+
--- scripts/summary/guest_summary.js
+++ scripts/summary/guest_summary.js
@@ -1,93 +1,119 @@
 function toggleMenu() {
-    document.getElementById('submenu_toogle').classList.toggle('submenu_open');
+  document.getElementById('submenu_toogle').classList.toggle('submenu_open');
+}
+
+document.addEventListener('click', function(event) {
+  const submenu = document.getElementById('submenu_toogle');
+  const toggleButton = document.getElementById('guest_logo_user_sign_in');
+
+  if (!submenu.contains(event.target) && !toggleButton.contains(event.target)) {
+    submenu.classList.remove('submenu_open'); 
   }
-  
-  document.addEventListener('click', function(event) {
-    const submenu = document.getElementById('submenu_toogle');
-    const toggleButton = document.getElementById('guest_logo_user_sign_in');
-  
-    if (!submenu.contains(event.target) && !toggleButton.contains(event.target)) {
-      submenu.classList.remove('submenu_open'); 
-    }
-  });
-  
-  const Base_URL = "https://joinstorage-805e6-default-rtdb.europe-west1.firebasedatabase.app/";
+});
+
+const Base_URL = "https://joinstorage-805e6-default-rtdb.europe-west1.firebasedatabase.app/";
 
 let currentTasks = [];
 
 async function fetchData() {
-  let TaskResponse = await fetch(`${Base_URL}/tasks.json`);
-  TaskResponse = await TaskResponse.json();
-  // Speichern der Aufgaben im Array
-  currentTasks = Object.values(TaskResponse);
+let TaskResponse = await fetch(`${Base_URL}/tasks.json`);
+TaskResponse = await TaskResponse.json();
+// Speichern der Aufgaben im Array
+currentTasks = Object.values(TaskResponse);
 
-  console.log(currentTasks);
-  update()
+console.log(currentTasks);
+update()
 }
 
-function update(){
-  updateHTML();
-  renderWelcome();
+function update() {
+if (isUserLoggedIn() && localStorage.getItem('greetingShown') === 'false') {
+    showGreetingContainer();
+}
+updateHTML();
+renderWelcome();
+}
+
+function showGreetingContainer() {
+if (window.innerWidth < 1000) {
+    const mobileGuestContainer = document.querySelector('.mobile_container-morning');
+    mobileGuestContainer.style.display = 'flex';
+
+    setTimeout(() => {
+        mobileGuestContainer.style.display = 'none';
+        localStorage.setItem('greetingShown', 'true');
+    }, 3000);
+}
+}
+
+function isUserLoggedIn() {
+return localStorage.getItem('userLoggedIn') === 'true';
+}
+
+function logoutGuest() {
+localStorage.removeItem('userLoggedIn');
+localStorage.setItem('greetingShown', 'false');
 }
 
 function openPage(){
-  window.location.href = "../HTML/board.html"; 
+window.location.href = "../HTML/board.html"; 
 }
 
 function updateHTML() {
-  document.getElementById("content_urgent").innerHTML = getNumberToDo();
-  document.getElementById("content_to_do").innerHTML = getNumberToDo();
-  document.getElementById("content_success").innerHTML = getNumberSuccess();
-  document.getElementById("content_date_div").innerHTML = getDeadlineDate();
-  document.getElementById("content_task_in_board").innerHTML = getNumberTaskInBoard();
-  document.getElementById("content_task_in_progress").innerHTML = getNumberTaskInProgress();
-  document.getElementById("content_awaiting_feedback").innerHTML = getNumberAwaitingFeedback();
+document.getElementById("content_urgent").innerHTML = getNumberToDo();
+document.getElementById("content_to_do").innerHTML = getNumberToDo();
+document.getElementById("content_success").innerHTML = getNumberSuccess();
+document.getElementById("content_date_div").innerHTML = getDeadlineDate();
+document.getElementById("content_task_in_board").innerHTML = getNumberTaskInBoard();
+document.getElementById("content_task_in_progress").innerHTML = getNumberTaskInProgress();
+document.getElementById("content_awaiting_feedback").innerHTML = getNumberAwaitingFeedback();
 }
 
 function getNumberUrgent() {
-  return currentTasks.filter(task => task && task.status === "urgent").length;
+return currentTasks.filter(task => task && task.status === "urgent").length;
 }
 
 function getNumberToDo() {
-  return currentTasks.filter(task => task && task.status === "toDo").length;
+return currentTasks.filter(task => task && task.status === "toDo").length;
 }
 
 
 function getNumberSuccess() {
-  return currentTasks.filter(task => task && task.status === "done").length;
+return currentTasks.filter(task => task && task.status === "done").length;
 }
 
 function getDeadlineDate() {
-  const upcomingTask = currentTasks.find(task => task && task.deadline && task.deadline !== "");
-  return upcomingTask ? upcomingTask.deadline : "Keine Deadline festgelegt";
+const upcomingTask = currentTasks.find(task => task && task.deadline && task.deadline !== "");
+return upcomingTask ? upcomingTask.deadline : "Keine Deadline festgelegt";
 }
 
 function getNumberTaskInBoard() {
-  return currentTasks.length;
+return currentTasks.length;
 }
 
 function getNumberTaskInProgress() {
-  return currentTasks.filter(task => task && task.status === "inProgress").length;
+return currentTasks.filter(task => task && task.status === "inProgress").length;
 }
 
 function getNumberAwaitingFeedback() {
-  return currentTasks.filter(task => task && task.status === "awaitFeedback").length;
+return currentTasks.filter(task => task && task.status === "awaitFeedback").length;
 }
 
 function renderWelcome() {
-  let currentWelcome = document.getElementById('content_welcome');
-  
-  const currentHour = new Date().getHours(); 
+let currentWelcome = document.getElementById('content_welcome');
+let mobileCurrentWelcome = document.getElementById('mobie_content_welcome');
 
-  let greeting = '';
-  if (currentHour >= 5 && currentHour < 12) {
-      greeting = 'Good morning';
-  } else if (currentHour >= 12 && currentHour < 18) {
-      greeting = 'Good afternoon';
-  } else {
-      greeting = 'Good evening';
-  }
-  currentWelcome.textContent = greeting;
+const currentHour = new Date().getHours(); 
+
+let greeting = '';
+if (currentHour >= 5 && currentHour < 12) {
+    greeting = 'Good morning,';
+} else if (currentHour >= 12 && currentHour < 18) {
+    greeting = 'Good afternoon,';
+} else {
+    greeting = 'Good evening,';
+}
+currentWelcome.textContent = greeting;
+mobileCurrentWelcome.textContent = greeting;
 }
 
 
