d9d2814e1fb7d55a356209b6c9677fa8f9021aa1
eb.developer87@gmail.com <eb.developer87@gmail.com>
2025-04-02 17:41:27 +0200

    login_guest js code commented

--- scripts/login/login_guest.js
+++ scripts/login/login_guest.js
@@ -1,35 +1,34 @@
-async function guestLogin() {
-    if (!localStorage.getItem('userLoggedIn')) {
-        localStorage.setItem('userLoggedIn', 'true');
-    }
-    if (!localStorage.getItem('greetingShown')) {
-        localStorage.setItem('greetingShown', 'false');
-    }
-    openGuestLoginPage();
-}
-
-
-function openGuestLoginPage() {
-    try {
-        const url = "../HTML/summary_guest.html";
-        if (url) {
-            window.location.href = url;
-        } else {
-            throw new Error('Ungültige URL');
-        }
-    } catch (error) {
-        console.error("Fehler bei der Weiterleitung:", error);
-    }
-}
-
-
+/**
+ * This function is used to log in a guest user
+ * It checks if the user is already logged in and sets necessary flags in localStorage
+ * Sets the user login status and greeting flag for the guest
+ */
 async function guestLogin() {
     if (localStorage.getItem('userLoggedIn') === 'true') {
-        console.log("Der Benutzer ist bereits eingeloggt.");
+        console.log("The user is already logged in.");
         return; 
     }
     localStorage.setItem('userLoggedIn', 'true');
     localStorage.setItem('greetingShown', 'false');
+    
     openGuestLoginPage();
 }
+
+/**
+ * This function opens the guest login page by redirecting the browser
+ * @throws {Error} Throws an error if the URL is invalid
+ */
+function openGuestLoginPage() {
+    try {
+        const url = "../HTML/summary_guest.html";
+        if (url) {
+            window.location.href = url; 
+        } else {
+            throw new Error('Invalid URL'); 
+        }
+    } catch (error) {
+        console.error("Error during redirection:", error);
+    }
+}
+
 
